9a29fbeeb0d656c5d4c7649c6a68c66ce4df4eb4
akani mashaba <akanimmashaba@gmail.com>
2023-12-22 14:50:43 +0200

    routes

---
+++ src/app/varsity/page.tsx
@@ -0,0 +1,16 @@
+import Image from 'next/image'
+import LandingSnapx from '@/components/sections/landing-snap';
+
+
+export default function Home() {
+  return (
+    // mx-auto max-w-7xl px-2 sm:px-6 lg:px-8
+    <div className="">
+    
+      
+     blog list
+
+   </div>
+
+  )
+}
